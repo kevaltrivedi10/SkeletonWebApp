3cc0c9a8bf21add9e6d5aab990649c968fbe00bb
kevaltrivedi10 <135966976+kevaltrivedi10@users.noreply.github.com>
2023-06-13 14:58:29 +0000

    query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,10 +6,8 @@ public class QueryProcessor {
 
     System.out.println("Received query:" + query);
 
-    if (query.toLowerCase().contains("shakespeare")) {
-      return "William Shakespeare (26 April 1564 - 23 April 1616) was an "
-          + "English poet, playwright, and actor, widely regarded as the greatest "
-          + "writer in the English language and the world's pre-eminent dramatist.";
+    if (query.toLowerCase().contains("What is 85 minus 95?\t")) {
+      return "-10";
     }
 
     if (query.toLowerCase().contains("Keval")) {
